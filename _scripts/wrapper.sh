bcd21dfd8257abc0a9e244d4009b5a035669c2c8
psi <ryo.hirafuji@link-u.co.jp>
2019-11-20 02:16:48 +0900

    macはreadlink -fがない。

--- _scripts/wrapper.sh
+++ _scripts/wrapper.sh
@@ -1,5 +1,26 @@
 #! /bin/bash -eu
-SCRIPT_PATH=$(readlink -f $0)
+
+# from: https://qiita.com/edvakf@github/items/b8400f7dfe9210aadddd
+function readlink_f() {
+  local TARGET_FILE=$1
+  cd $(dirname ${TARGET_FILE})
+  TARGET_FILE=$(basename ${TARGET_FILE})
+
+  # Iterate down a (possible) chain of symlinks
+  while [ -L "${TARGET_FILE}" ]
+  do
+      TARGET_FILE=$(readlink ${TARGET_FILE})
+      cd $(dirname ${TARGET_FILE})
+      TARGET_FILE=$(basename ${TARGET_FILE})
+  done
+
+  # Compute the canonicalized name by finding the physical path
+  # for the directory we're in and appending the target file.
+  local PHYS_DIR=$(pwd -P)
+  echo ${PHYS_DIR}/${TARGET_FILE}
+}
+
+SCRIPT_PATH=$(readlink_f $0)
 SCRIPT_DIR=$(cd $(dirname ${SCRIPT_PATH}); pwd)
 
 REQUIREMENT_HASH=$(cat ${SCRIPT_DIR}/req.txt | openssl dgst -md5 | sed 's/^.* //')
