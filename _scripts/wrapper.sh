8001dd044de5a66d39a7c4cc8d62f59a4b75b458
y-hashida <49150386+y-hashida@users.noreply.github.com>
2020-10-29 14:24:15 +0900

    ansible-galaxy コマンドのローカル化とその他のユーティリティ周りに機能追加. (#5)
    * update: wrapper.sh に以下の機能を追加
    * wrapper.sh をそのまま実行した場合, ansible のインストールのみを実行する.
    * 環境変数 ANSIBLE_COLLECTIONS_PATHS に _venvs 以下のディレクトリを追加.
    * 参考:
      * https://docs.ansible.com/ansible/latest/reference_appendices/config.html#collections-paths
      * https://docs.ansible.com/ansible/latest/galaxy/user_guide.html#installing-a-collection-from-galaxy
    
    * add: ansible-doc コマンドを用意.
    
    * add: _venvs をクリーンにするスクリプトを追加.
    
    * add: ansible をインストールするシェルスクリプトを用意.
    
    * update: デフォルトの req.txt を以下のものに更新.
    * ansible から ansible-base に変更. バージョンは 2.10系に更新.
    * Jinja2 のバージョンを 2.11系に更新.
    
    * update: コメントにミスが有ったので修正.
    
    * add: ansible-config コマンドを用意
    
    * update: 不要な修正を削除.

--- _scripts/wrapper.sh
+++ _scripts/wrapper.sh
@@ -2,37 +2,58 @@
 
 # from: https://qiita.com/edvakf@github/items/b8400f7dfe9210aadddd
 function readlink_f() {
-  local TARGET_FILE=$1
-  cd $(dirname ${TARGET_FILE})
-  TARGET_FILE=$(basename ${TARGET_FILE})
+    local TARGET_FILE=$1
+    cd $(dirname ${TARGET_FILE})
+    TARGET_FILE=$(basename ${TARGET_FILE})
 
-  # Iterate down a (possible) chain of symlinks
-  while [ -L "${TARGET_FILE}" ]
-  do
-      TARGET_FILE=$(readlink ${TARGET_FILE})
-      cd $(dirname ${TARGET_FILE})
-      TARGET_FILE=$(basename ${TARGET_FILE})
-  done
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "${TARGET_FILE}" ]
+    do
+        TARGET_FILE=$(readlink ${TARGET_FILE})
+        cd $(dirname ${TARGET_FILE})
+        TARGET_FILE=$(basename ${TARGET_FILE})
+    done
 
-  # Compute the canonicalized name by finding the physical path
-  # for the directory we're in and appending the target file.
-  local PHYS_DIR=$(pwd -P)
-  echo ${PHYS_DIR}/${TARGET_FILE}
+    # Compute the canonicalized name by finding the physical path
+    # for the directory we're in and appending the target file.
+    local PHYS_DIR=$(pwd -P)
+    echo ${PHYS_DIR}/${TARGET_FILE}
 }
 
 SCRIPT_PATH=$(readlink_f $0)
 SCRIPT_DIR=$(cd $(dirname ${SCRIPT_PATH}); pwd)
 
 if [ -e "${SCRIPT_DIR}/../../req.txt" ]; then
-  REQ_TXT="${SCRIPT_DIR}/../../req.txt"
+    REQ_TXT="${SCRIPT_DIR}/../../req.txt"
 else
-  REQ_TXT=${SCRIPT_DIR}/req.txt
+    REQ_TXT=${SCRIPT_DIR}/req.txt
 fi
 
 REQUIREMENT_HASH=$(cat ${REQ_TXT} | openssl dgst -md5 | sed 's/^.* //')
 VENV_DIR=$(cd ${SCRIPT_DIR}/../_venvs; pwd)/${REQUIREMENT_HASH}
 
+## python3-venv の作成とアクティベート
 bash ${SCRIPT_DIR}/prepare.sh ${VENV_DIR}
 source ${VENV_DIR}/bin/activate
 
-${VENV_DIR}/bin/$(basename $0) "$@"
+## ansible collections path の設定
+#  * VENV_COLLECTIONS_PATH を ANSIBLE_COLLECTIONS_PATHS の先頭に追加
+#  * VENV_COLLECTIONS_PATH ディレクトリを作成
+#  * ANSIBLE_COLLECTIONS_PATHS を環境変数としてエクスポート
+#  * 参照: https://docs.ansible.com/ansible/latest/reference_appendices/config.html#collections-paths
+DEFAULT_COLLECTIONS_PATHS="${HOME}/.ansible/collections:/usr/share/ansible/collections"
+VENV_COLLECTIONS_PATH="${VENV_DIR}/share/ansible/collections"
+mkdir -p "${VENV_COLLECTIONS_PATH}"
+export ANSIBLE_COLLECTIONS_PATHS="${VENV_COLLECTIONS_PATH}:${DEFAULT_COLLECTIONS_PATHS}"
+
+## スクリプト名とコマンドライン引数を取得
+WRAPPER_CMD_NAME="$(basename ${0})"
+ARGS=("$@")
+
+## スクリプト名 が wrapper.sh の時はここで終了.
+if [[ "${WRAPPER_CMD_NAME}" == "wrapper.sh" ]]; then
+    exit 0;
+fi
+
+## シンボリック名に合わせて実行このスクリプトを実行
+"${VENV_DIR}/bin/${WRAPPER_CMD_NAME}" "${ARGS[@]}"
